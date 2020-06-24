f9a9c71bb62f7c33a4d9f8c9a2495e5f802113d8
Iliya Sobolev <soboli@att.net>
2020-06-24 15:35:54 -0500

    git commit -a

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
 var config = require('./config.dev');
 var apiUsersRouter = require('./routes/api/users');
 
