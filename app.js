97310c1587c4cd6313ea7afce6ae3943b299ba2a
Iliya Sobolev <soboli@att.net>
2020-06-24 13:59:17 -0500

    Implement a route for api/users

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mongoose = require('mongoose');
 var config = require('./config.dev');
+var apiUsersRouter = require('./routes/api/users');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/users', apiUsersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
