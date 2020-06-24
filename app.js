345a8b12deb179dc1539dbe9dea765d0c246bca0
Iliya Sobolev <soboli@att.net>
2020-06-24 13:41:17 -0500

    Install Mongoose

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 //Test the file
-console.log(config);
+//console.log(config);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
