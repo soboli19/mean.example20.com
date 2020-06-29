f6a27256c79c5323431188614c5477656447eae1
Iliya Sobolev <soboli@att.net>
2020-06-29 16:01:09 -0500

    Add a non-API endpoint for accessing the user app

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var express = require('express');
+var router = express.Router();
+
+router.get('/app', function(req, res, next) {
+  res.render('users/app', { title: 'User Management' });
 });
+
 
 module.exports = router;
